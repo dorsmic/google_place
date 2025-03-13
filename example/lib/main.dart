97c81858a095e4f68523be10a5f62a363faa91b8
RAYNIS <raynis@MacBook-Pro-de-RAYNIS.local>
2025-03-13 11:03:47 +0100

    upfate to flutter 3_29

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -6,11 +6,13 @@ import 'package:google_place/google_place.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await DotEnv().load('.env');
-  runApp(MyApp());
+  await dotenv.load(fileName: '.env');
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -18,23 +20,25 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: HomePage(),
+      home: const HomePage(),
     );
   }
 }
 
 class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
   @override
   _HomePageState createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
-  GooglePlace googlePlace;
+  late GooglePlace googlePlace;
   List<AutocompletePrediction> predictions = [];
 
   @override
   void initState() {
-    String apiKey = DotEnv().env['API_KEY'];
+    String apiKey = dotenv.env['API_KEY']!;
     googlePlace = GooglePlace(apiKey);
     super.initState();
   }
@@ -44,7 +48,7 @@ class _HomePageState extends State<HomePage> {
     return Scaffold(
       body: SafeArea(
         child: Container(
-          margin: EdgeInsets.only(right: 20, left: 20, top: 20),
+          margin: const EdgeInsets.only(right: 20, left: 20, top: 20),
           child: Column(
             mainAxisSize: MainAxisSize.min,
             children: <Widget>[
@@ -68,7 +72,7 @@ class _HomePageState extends State<HomePage> {
                   if (value.isNotEmpty) {
                     autoCompleteSearch(value);
                   } else {
-                    if (predictions.length > 0 && mounted) {
+                    if (predictions.isNotEmpty && mounted) {
                       setState(() {
                         predictions = [];
                       });
@@ -76,7 +80,7 @@ class _HomePageState extends State<HomePage> {
                   }
                 },
               ),
-              SizedBox(
+              const SizedBox(
                 height: 10,
               ),
               Expanded(
@@ -84,31 +88,34 @@ class _HomePageState extends State<HomePage> {
                   itemCount: predictions.length,
                   itemBuilder: (context, index) {
                     return ListTile(
-                      leading: CircleAvatar(
+                      leading: const CircleAvatar(
                         child: Icon(
                           Icons.pin_drop,
                           color: Colors.white,
                         ),
                       ),
-                      title: Text(predictions[index].description),
+                      title: Text(predictions[index].description ?? ''),
                       onTap: () {
-                        debugPrint(predictions[index].placeId);
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) => DetailsPage(
-                              placeId: predictions[index].placeId,
-                              googlePlace: googlePlace,
+                        final placeId = predictions[index].placeId;
+                        if (placeId != null) {
+                          debugPrint(placeId);
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => DetailsPage(
+                                placeId: placeId,
+                                googlePlace: googlePlace,
+                              ),
                             ),
-                          ),
-                        );
+                          );
+                        }
                       },
                     );
                   },
                 ),
               ),
               Container(
-                margin: EdgeInsets.only(top: 10, bottom: 10),
+                margin: const EdgeInsets.only(top: 10, bottom: 10),
                 child: Image.asset("assets/powered_by_google.png"),
               ),
             ],
@@ -122,7 +129,7 @@ class _HomePageState extends State<HomePage> {
     var result = await googlePlace.autocomplete.get(value);
     if (result != null && result.predictions != null && mounted) {
       setState(() {
-        predictions = result.predictions;
+        predictions = result.predictions!;
       });
     }
   }
@@ -132,25 +139,24 @@ class DetailsPage extends StatefulWidget {
   final String placeId;
   final GooglePlace googlePlace;
 
-  DetailsPage({Key key, this.placeId, this.googlePlace}) : super(key: key);
+  const DetailsPage({
+    Key? key,
+    required this.placeId,
+    required this.googlePlace,
+  }) : super(key: key);
 
   @override
-  _DetailsPageState createState() =>
-      _DetailsPageState(this.placeId, this.googlePlace);
+  _DetailsPageState createState() => _DetailsPageState();
 }
 
 class _DetailsPageState extends State<DetailsPage> {
-  final String placeId;
-  final GooglePlace googlePlace;
-
-  _DetailsPageState(this.placeId, this.googlePlace);
-
-  DetailsResult detailsResult;
+  late DetailsResult? detailsResult;
   List<Uint8List> images = [];
 
   @override
   void initState() {
-    getDetils(this.placeId);
+    detailsResult = null;
+    getDetails(widget.placeId);
     super.initState();
   }
 
@@ -158,19 +164,19 @@ class _DetailsPageState extends State<DetailsPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Details"),
+        title: const Text("Details"),
         backgroundColor: Colors.blueAccent,
       ),
       floatingActionButton: FloatingActionButton(
         backgroundColor: Colors.blueAccent,
         onPressed: () {
-          getDetils(this.placeId);
+          getDetails(widget.placeId);
         },
-        child: Icon(Icons.refresh),
+        child: const Icon(Icons.refresh),
       ),
       body: SafeArea(
         child: Container(
-          margin: EdgeInsets.only(right: 20, left: 20, top: 20),
+          margin: const EdgeInsets.only(right: 20, left: 20, top: 20),
           child: Column(
             mainAxisSize: MainAxisSize.min,
             children: <Widget>[
@@ -199,7 +205,7 @@ class _DetailsPageState extends State<DetailsPage> {
                   },
                 ),
               ),
-              SizedBox(
+              const SizedBox(
                 height: 10,
               ),
               Expanded(
@@ -211,8 +217,8 @@ class _DetailsPageState extends State<DetailsPage> {
                   child: ListView(
                     children: <Widget>[
                       Container(
-                        margin: EdgeInsets.only(left: 15, top: 10),
-                        child: Text(
+                        margin: const EdgeInsets.only(left: 15, top: 10),
+                        child: const Text(
                           "Details",
                           style: TextStyle(
                             fontSize: 20,
@@ -220,98 +226,91 @@ class _DetailsPageState extends State<DetailsPage> {
                           ),
                         ),
                       ),
-                      detailsResult != null && detailsResult.types != null
-                          ? Container(
-                              margin: EdgeInsets.only(left: 15, top: 10),
-                              height: 50,
-                              child: ListView.builder(
-                                scrollDirection: Axis.horizontal,
-                                itemCount: detailsResult.types.length,
-                                itemBuilder: (context, index) {
-                                  return Container(
-                                    margin: EdgeInsets.only(right: 10),
-                                    child: Chip(
-                                      label: Text(
-                                        detailsResult.types[index],
-                                        style: TextStyle(
-                                          color: Colors.white,
-                                        ),
-                                      ),
-                                      backgroundColor: Colors.blueAccent,
+                      if (detailsResult?.types != null)
+                        Container(
+                          margin: const EdgeInsets.only(left: 15, top: 10),
+                          height: 50,
+                          child: ListView.builder(
+                            scrollDirection: Axis.horizontal,
+                            itemCount: detailsResult!.types!.length,
+                            itemBuilder: (context, index) {
+                              return Container(
+                                margin: const EdgeInsets.only(right: 10),
+                                child: Chip(
+                                  label: Text(
+                                    detailsResult!.types![index],
+                                    style: const TextStyle(
+                                      color: Colors.white,
                                     ),
-                                  );
-                                },
-                              ),
-                            )
-                          : Container(),
+                                  ),
+                                  backgroundColor: Colors.blueAccent,
+                                ),
+                              );
+                            },
+                          ),
+                        ),
                       Container(
-                        margin: EdgeInsets.only(left: 15, top: 10),
+                        margin: const EdgeInsets.only(left: 15, top: 10),
                         child: ListTile(
-                          leading: CircleAvatar(
+                          leading: const CircleAvatar(
                             child: Icon(Icons.location_on),
                           ),
                           title: Text(
-                            detailsResult != null &&
-                                    detailsResult.formattedAddress != null
-                                ? 'Address: ${detailsResult.formattedAddress}'
-                                : "Address: null",
+                            detailsResult?.formattedAddress != null
+                                ? 'Address: ${detailsResult!.formattedAddress}'
+                                : "Address: Not available",
                           ),
                         ),
                       ),
                       Container(
-                        margin: EdgeInsets.only(left: 15, top: 10),
+                        margin: const EdgeInsets.only(left: 15, top: 10),
                         child: ListTile(
-                          leading: CircleAvatar(
+                          leading: const CircleAvatar(
                             child: Icon(Icons.location_searching),
                           ),
                           title: Text(
-                            detailsResult != null &&
-                                    detailsResult.geometry != null &&
-                                    detailsResult.geometry.location != null
-                                ? 'Geometry: ${detailsResult.geometry.location.lat.toString()},${detailsResult.geometry.location.lng.toString()}'
-                                : "Geometry: null",
+                            detailsResult?.geometry?.location != null
+                                ? 'Geometry: ${detailsResult!.geometry!.location!.lat},${detailsResult!.geometry!.location!.lng}'
+                                : "Geometry: Not available",
                           ),
                         ),
                       ),
                       Container(
-                        margin: EdgeInsets.only(left: 15, top: 10),
+                        margin: const EdgeInsets.only(left: 15, top: 10),
                         child: ListTile(
-                          leading: CircleAvatar(
+                          leading: const CircleAvatar(
                             child: Icon(Icons.timelapse),
                           ),
                           title: Text(
-                            detailsResult != null &&
-                                    detailsResult.utcOffset != null
-                                ? 'UTC offset: ${detailsResult.utcOffset.toString()} min'
-                                : "UTC offset: null",
+                            detailsResult?.utcOffset != null
+                                ? 'UTC offset: ${detailsResult!.utcOffset} min'
+                                : "UTC offset: Not available",
                           ),
                         ),
                       ),
                       Container(
-                        margin: EdgeInsets.only(left: 15, top: 10),
+                        margin: const EdgeInsets.only(left: 15, top: 10),
                         child: ListTile(
-                          leading: CircleAvatar(
+                          leading: const CircleAvatar(
                             child: Icon(Icons.rate_review),
                           ),
                           title: Text(
-                            detailsResult != null &&
-                                    detailsResult.rating != null
-                                ? 'Rating: ${detailsResult.rating.toString()}'
-                                : "Rating: null",
+                            detailsResult?.rating != null
+                                ? 'Rating: ${detailsResult!.rating}'
+                                : "Rating: Not available",
                           ),
                         ),
                       ),
                       Container(
-                        margin: EdgeInsets.only(left: 15, top: 10),
+                        margin: const EdgeInsets.only(left: 15, top: 10),
                         child: ListTile(
-                          leading: CircleAvatar(
+                          leading: const CircleAvatar(
                             child: Icon(Icons.attach_money),
                           ),
                           title: Text(
-                            detailsResult != null &&
-                                    detailsResult.priceLevel != null
-                                ? 'Price level: ${detailsResult.priceLevel.toString()}'
-                                : "Price level: null",
+                            detailsResult?.priceLevel != null
+                                ? 'Price level: ${detailsResult!.priceLevel}'
+                                : "Price level: Not available",
                           ),
                         ),
                       ),
@@ -320,7 +319,7 @@ class _DetailsPageState extends State<DetailsPage> {
                 ),
               ),
               Container(
-                margin: EdgeInsets.only(top: 20, bottom: 10),
+                margin: const EdgeInsets.only(top: 20, bottom: 10),
                 child: Image.asset("assets/powered_by_google.png"),
               ),
             ],
@@ -330,24 +329,26 @@ class _DetailsPageState extends State<DetailsPage> {
     );
   }
 
-  void getDetils(String placeId) async {
-    var result = await this.googlePlace.details.get(placeId);
+  void getDetails(String placeId) async {
+    var result = await widget.googlePlace.details.get(placeId);
     if (result != null && result.result != null && mounted) {
       setState(() {
         detailsResult = result.result;
         images = [];
       });
 
-      if (result.result.photos != null) {
-        for (var photo in result.result.photos) {
-          getPhoto(photo.photoReference);
+      if (result.result?.photos != null) {
+        for (var photo in result.result!.photos!) {
+          if (photo.photoReference != null) {
+            getPhoto(photo.photoReference!);
+          }
         }
       }
     }
   }
 
   void getPhoto(String photoReference) async {
-    var result = await this.googlePlace.photos.get(photoReference, null, 400);
+    var result = await widget.googlePlace.photos.get(photoReference, 0, 400);
     if (result != null && mounted) {
       setState(() {
         images.add(result);
